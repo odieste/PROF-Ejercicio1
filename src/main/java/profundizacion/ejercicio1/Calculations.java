c2f3b7f0af5faa2e891b3bca21c29a4409be27aa
odieste <odieste@fi.upm.es>
2021-11-02 17:35:29 +0100

    Throw an exception if the argument is smaller than or equal to zero

--- src/main/java/profundizacion/ejercicio1/Calculations.java
+++ src/main/java/profundizacion/ejercicio1/Calculations.java
@@ -2,7 +2,10 @@ package profundizacion.ejercicio1;
 
 public class Calculations {
 	
-	public void factorial(int i) {
+	public void factorial(int i) throws TooSmallNumberException {
+		
+		// 2. Throw an exception if the argument is smaller than or equal to zero
+		if(i <= 0) throw new TooSmallNumberException();
 		
 	}
 
